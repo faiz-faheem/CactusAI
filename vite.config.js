d01305ef3cb30c9e9ac64c9f737475bb991d4c53
faiz <faizfaheem89@gmail.com>
2025-01-14 18:00:04 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/CactusAI",
-=======
-  base:"/CactusAI/",
->>>>>>> 4d326e762d6ff6817035ac1bd040944bd442dc84
+  base: "/CactusAI",
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
